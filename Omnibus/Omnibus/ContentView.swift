58396936ce85d6ed37378e5a23123c8260bd1345
simba531 <kdh5604123@gmail.com>
2022-09-21 16:38:47 +0900

    [2022.09.21] Start

---
+++ Omnibus/Omnibus/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Omnibus
+//
+//  Created by 김도현 on 2022/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Omnibus/Omnibus/OmnibusApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmnibusApp.swift
+//  Omnibus
+//
+//  Created by 김도현 on 2022/09/21.
+//
+
+import SwiftUI
+
+@main
+struct OmnibusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
